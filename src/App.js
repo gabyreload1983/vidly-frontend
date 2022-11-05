7025474320ee672d1cd8838430b12d5bc00b29e4
Gabriel Godoy <gabyreload@gmail.com>
2022-11-05 15:34:24 -0300

    Routing
    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Customers from "./components/customers";
 import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
-    <main className="container">
-      <Movies></Movies>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
