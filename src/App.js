98cc6fe0e49a44d03cf5d9334cf5c9c25b857826
Gabriel Godoy <gabyreload@gmail.com>
2022-11-06 00:41:09 -0300

    Forms
    login and register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import "./App.css";
 import Customers from "./components/customers";
 import Movies from "./components/movies";
 import NavBar from "./components/navbar";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/login";
+import "./App.css";
+import RegisterForm from "./components/registerForm";
 
 function App() {
   return (
@@ -14,10 +16,12 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
+          <Route path="/register" component={RegisterForm} />
           <Route path="/not-found" component={NotFound} />
           <Redirect from="/" exact to="/movies" />
           <Redirect to="/not-found" />
