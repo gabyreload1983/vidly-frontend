50cadbec0f61df91aaba8c7674cabaa1c277967d
Gabriel Godoy <gabyreload@gmail.com>
2022-11-09 23:17:42 -0300

    Calling protected API endpoints

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,6 +5,8 @@ import jwtDecode from "jwt-decode";
 const apiEndpoint = `${config.apiUrl}/auth`;
 const tokenKey = "token";
 
+http.setJwt(getJwt());
+
 export async function login(email, password) {
   const { data: jwt } = await http.post(apiEndpoint, {
     email,
@@ -30,9 +32,14 @@ export function getCurrentUser() {
   }
 }
 
+export function getJwt() {
+  return localStorage.getItem(tokenKey);
+}
+
 export default {
   login,
   logout,
   getCurrentUser,
   loginWithJwt,
+  getJwt,
 };
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { toast } from "react-toastify";
 import logger from "./logService";
+import { toast } from "react-toastify";
 
 axios.interceptors.response.use(null, (error) => {
   const expetedError =
@@ -15,9 +15,14 @@ axios.interceptors.response.use(null, (error) => {
   return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
 export default {
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete,
+  setJwt,
 };
