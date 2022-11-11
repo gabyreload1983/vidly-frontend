36c781feea1c55b3ea97df7e6318f3d138ab2c84
Gabriel Godoy <gabyreload@gmail.com>
2022-11-10 21:20:58 -0300

    Hiding the Delete button

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import auth from "../services/authService";
 import Like from "./common/like";
 import Table from "./common/table";
 
@@ -21,20 +22,26 @@ class MoviesTable extends Component {
         <Like onLike={() => this.props.onLike(movie)} liked={movie.liked} />
       ),
     },
-    {
-      key: "delete",
-      content: (movie) => (
-        <button
-          onClick={() => {
-            this.props.onDelete(movie);
-          }}
-          className="btn btn-sm btn-danger"
-        >
-          Delete
-        </button>
-      ),
-    },
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: (movie) => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-sm btn-danger"
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+  }
+
   render() {
     const { movies, onSort, sortColumn } = this.props;
     return (
